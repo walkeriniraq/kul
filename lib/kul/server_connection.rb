9ac355e73cedb428a96906d660eb384b212d9e4c
Nathan Walker <nathan@rylath.net>
2013-03-25 01:12:06 -0400

    Think I like this way better for dealing with the server connection

--- lib/kul/server_connection.rb
+++ lib/kul/server_connection.rb
@@ -1,9 +1,9 @@
-module Kul::ServerConnection
-  def self.route_path(path, params)
+class Kul::ServerConnection
+  def route_path(path, params)
     raise 'Not implemented'
   end
 
-  def self.route_action(params)
+  def route_action(params)
     raise 'Not implemented'
   end
 end
