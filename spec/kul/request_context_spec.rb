a4570e913ae09856d5a0bb9701e43acaaccd73b9
Nathan Walker <nathan@rylath.net>
2013-04-23 18:12:44 -0400

    Implemented the render_template test

--- spec/kul/request_context_spec.rb
+++ spec/kul/request_context_spec.rb
@@ -2,6 +2,12 @@ require 'spec_helper'
 
 describe Kul::RequestContext do
   context '#render_template' do
-    it 'renders the template with itself as the context'
+    it 'renders the template with itself as the context' do
+      context = Kul::RequestContext.new
+      test = context.render_template 'foo'
+      test.should be
+      test.file.should == 'foo'
+      test.context.should == context
+    end
   end
 end
