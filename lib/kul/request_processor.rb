ecf73635cfc30142b7c077807ddb6ca89f6c7481
Nathan Walker <nathan@rylath.net>
2013-05-07 22:38:38 -0400

    Came up with a really neat way of testing the request processor

--- lib/kul/request_processor.rb
+++ lib/kul/request_processor.rb
@@ -1,48 +1,48 @@
+require 'kul/request_context'
 require 'kul/framework_factory'
-require 'kul/route_listing'
 require 'sass'
 
 # this class handles the sinatra request and builds the request context
 class Kul::RequestProcessor < Sinatra::Base
-  set :extension_includes, ['html', 'js', 'css']
+  set :extension_includes, %w(html js css)
 
   get '/favicon.ico' do
-    send_file "favicon.ico"
+    send_file 'favicon.ico'
   end
 
-  get '/*.:extension' do
-    puts params.to_s
-    Kul::RequestContext.new :path => params[:splat].first, :extension => params[:extension], :params => params
+  get '/:app/:controller/:action.:extension' do
+    Kul::RequestProcessor.do_route :app => params[:app], :controller => params[:controller], :action => params[:action], :extension => params[:extension], :params => params
   end
 
   get '/:app/:controller/:action' do
-    Kul::FrameworkFactory.create_server.route_action(Kul::RequestContext.new(params: params)).render
+    Kul::RequestProcessor.do_route :app => params[:app], :controller => params[:controller], :action => params[:action], :params => params
+  end
+
+  get '/:app/*.:extension' do |app, path, extension|
+    Kul::RequestProcessor.do_route :app => app, :path => path, :extension => extension, :params => params
+  end
+
+  get '/:path.:extension' do
+    Kul::RequestProcessor.do_route :path => params[:path], :extension => params[:extension], :params => params
   end
 
   get '/' do
-    try_render 'index', 'html' or ResponseNotFound.new.render
+    Kul::RequestProcessor.do_route :path => 'index', :extension => 'html', :params => params
   end
 
-  get '/*' do
-    try_render "#{params[:splat].first.to_s}/index", 'html' or ResponseNotFound.new.render
+  get '/:app' do
+    Kul::RequestProcessor.do_route :app => params[:app], :path => 'index', :extension => 'html', :params => params
   end
 
-  def try_render(file_path, extension)
-    return unless settings.extension_includes.include? extension
-    path = Pathname.new("#{file_path}.#{extension}")
-    send_file path.to_s if path.exist?
-    return case extension
-             when 'css'
-               render_if "#{file_path}.css.scss"
-             when 'js'
-               render_if "#{file_path}.js.coffee"
-             when 'html'
-               Kul::FrameworkFactory.create_server.route_path file_path, params
-           end
+  get '/:app/*' do |app, path|
+    Kul::RequestProcessor.do_route :app => app, :path => "#{path}/index", :extension => 'html', :params => params
   end
 
-  def render_if(path)
-    Tilt.new(path).render if File.exists? path
+  def self.do_route(context_params = {})
+    context = Kul::RequestContext.new context_params
+    response = Kul::FrameworkFactory.create_server.handle_request context
+    return response.render if response.respond_to? :render
+    response.to_s
   end
 
 end
--- spec/kul/request_processor_spec.rb
+++ spec/kul/request_processor_spec.rb
@@ -14,7 +14,6 @@ describe Kul::RequestProcessor do
         last_response.should be_ok
       end
     end
-
     it 'returns 404 when file is not present' do
       inside_empty_server do
         get '/favicon.ico'
@@ -24,22 +23,119 @@ describe Kul::RequestProcessor do
     end
   end
 
+  describe '#do_route' do
+    it 'builds a request context with the context_params' do
+      context  = double
+      response = double(:render => '')
+      server   = double(:handle_request => response)
+      Kul::FrameworkFactory.stub(:create_server).and_return server
+      Kul::RequestContext.should_receive(:new).with(:place => 'here', :time => 'now').and_return context
+      Kul::RequestProcessor.do_route :place => 'here', :time => 'now'
+    end
+    it 'passes the request context to a server' do
+      server   = double
+      context  = double
+      response = double(:render => '')
+      Kul::RequestContext.stub(:new).and_return context
+      Kul::FrameworkFactory.stub(:create_server).and_return server
+      server.should_receive(:handle_request).with(context).and_return response
+      Kul::RequestProcessor.do_route
+    end
+    it 'renders and returns the response from the server' do
+      response = double(:render => 'this is my test')
+      server   = double(:handle_request => response)
+      Kul::FrameworkFactory.stub(:create_server).and_return server
+      test = Kul::RequestProcessor.do_route
+      test.should == 'this is my test'
+    end
+    it 'returns response.to_s if response has no render method' do
+      response = double
+      server   = double(:handle_request => response)
+      Kul::FrameworkFactory.stub(:create_server).and_return server
+      response.should_receive(:to_s).and_return('this is a different test')
+      test = Kul::RequestProcessor.do_route
+      test.should == 'this is a different test'
+    end
+  end
+
+  context 'request for the root path' do
+    request_string = '/?foo=bar'
+    { path: 'index', extension: 'html', params: { 'foo' => 'bar'} }.each do |key, value|
+      it "sets #{key} to #{value}" do
+        Kul::RequestContext.should_receive(:new).with(hash_containing(key => value))
+        get request_string
+      end
+    end
+  end
+
+  context 'request for an app path' do
+    request_string = '/foo?bar=baz'
+    { app: 'foo', path: 'index', extension: 'html' }.each do |key, value|
+      it "sets #{key} to #{value}" do
+        Kul::RequestContext.should_receive(:new).with(hash_containing(key => value))
+        get request_string
+      end
+    end
+  end
+
   context 'request with a file and an extension' do
-    it 'creates a request context with :path' do
-      Kul::RequestContext.should_receive(:new).with(hash_including(:path => 'foo/bar')) {}
-      get '/foo/bar.html'
+    request_string = '/foo/bar.html?foo=bar&bar=baz'
+    { app: 'foo', path: 'bar', extension: 'html', params: { 'foo' => 'bar', 'bar' => 'baz' } }.each do |key, value|
+      it "sets #{key} to #{value}" do
+        Kul::RequestContext.should_receive(:new).with(hash_containing(key => value))
+        get request_string
+      end
     end
-    it 'creates a request context with :extension' do
-      Kul::RequestContext.should_receive(:new).with(hash_including(:extension => 'html')) {}
-      get '/foo/bar.html'
+  end
+
+  context 'request for an app sub-path' do
+    request_string = '/foo/bar?foo=bar&bar=baz'
+    { app: 'foo', path: 'bar/index', extension: 'html', params: { 'foo' => 'bar', 'bar' => 'baz' } }.each do |key, value|
+      it "sets #{key} to #{value}" do
+        Kul::RequestContext.should_receive(:new).with(hash_containing(key => value))
+        get request_string
+      end
     end
-    it 'creates a request context with :params' do
-      Kul::RequestContext.should_receive(:new).with(hash_containing(:params => { 'foo' => 'bar', 'bar' => 'baz' })) {}
-      get '/foo/bar.html?foo=bar&bar=baz'
+  end
+
+  context 'request for a controller route' do
+    request_string = '/foo/bar/baz?foo=bar&bar=baz'
+    { app: 'foo', controller: 'bar', action: 'baz', params: { 'foo' => 'bar', 'bar' => 'baz' } }.each do |key, value|
+      it "sets #{key} to #{value}" do
+        Kul::RequestContext.should_receive(:new).with(hash_containing(key => value))
+        get request_string
+      end
     end
-    it 'gets a router from the framework factory'
-    it 'passes the request context to the router'
-    it 'renders the response from the router'
+  end
+
+  context 'request for a controller route with an extension' do
+    request_string = '/foo/bar/baz.html?foo=bar&bar=baz'
+    { app: 'foo', controller: 'bar', action: 'baz', extension: 'html', params: { 'foo' => 'bar', 'bar' => 'baz' } }.each do |key, value|
+      it "sets #{key} to #{value}" do
+        Kul::RequestContext.should_receive(:new).with(hash_containing(key => value))
+        get request_string
+      end
+    end
+  end
+
+  context 'request for a folder path' do
+    request_string = '/foo/bar/baz/bat?foo=bar&bar=baz'
+    { app: 'foo', path: 'bar/baz/bat/index', extension: 'html', params: { 'foo' => 'bar', 'bar' => 'baz' } }.each do |key, value|
+      it "sets #{key} to #{value}" do
+        Kul::RequestContext.should_receive(:new).with(hash_containing(key => value))
+        get request_string
+      end
+    end
+  end
+
+  context 'request for a deep file path' do
+    request_string = '/foo/bar/baz/bat.html?foo=bar&bar=baz'
+    { app: 'foo', path: 'bar/baz/bat', extension: 'html', params: { 'foo' => 'bar', 'bar' => 'baz' } }.each do |key, value|
+      it "sets #{key} to #{value}" do
+        Kul::RequestContext.should_receive(:new).with(hash_containing(key => value))
+        get request_string
+      end
+    end
   end
 
 end
